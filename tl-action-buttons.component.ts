2dc1fb0613b9d4c42e0c00841be88ae6f0a0ed4f
RanjithGopi <101039505+Ranjithgopi25@users.noreply.github.com>
2026-02-25 16:38:55 +0530

    Update tl-action-buttons.component.ts

--- tl-action-buttons.component.ts
+++ tl-action-buttons.component.ts
@@ -1,2 +1,8 @@
-cd final_point/backend
+{
+      "offering_id": "L3_00106",
+      "offering_name": "Sales Technology - Salesforce Sales Cloud (CRM)",
+      "subtitle": "Accelerate CRM transformation with Salesforce Sales Cloud",
+      "description": "Implementation and configuration of Salesforce core Customer Relationship Management (CRM) capabilities via Salesforce Sales Cloud. This service enhances pipeline management, lead tracking, and customer relationships to streamline sales processes and increase efficiency."
+    }cd final_point/backend
 python -m app.features.chat.services.data_source_agent.commercial_hub_service "your query here"
+
